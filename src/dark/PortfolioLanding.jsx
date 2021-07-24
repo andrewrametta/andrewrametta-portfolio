189ce9f99487abddb6f82ef86135c9dd082110f6
Andrew <ramettaandrewj@gmail.com>
2021-07-24 17:27:56 -0400

    modified portfolio columns

--- src/dark/PortfolioLanding.jsx
+++ src/dark/PortfolioLanding.jsx
@@ -167,7 +167,7 @@ const PortfolioLanding = () => {
                 <PortfolioList
                   styevariation="text-center mt--40"
                   column="col-lg-4 col-md-6 col-sm-6 col-12"
-                  item="6"
+                  item="3"
                 />
               </div>
             </div>
