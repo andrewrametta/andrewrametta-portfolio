517c854191a8f122ce3556db3829925d09ef8cea
Andrew <ramettaandrewj@gmail.com>
2021-08-01 15:23:55 -0400

    build

---
+++ build/precache-manifest.90879cf4844614a09070bd8ce89af37e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ace08e8c9575c5a31a7a908f8439f18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a232888c8024a40c4a45",
+    "url": "/static/css/2.e3f8a3ed.chunk.css"
+  },
+  {
+    "revision": "9370d9801ef23d761b3d",
+    "url": "/static/css/main.ec29377e.chunk.css"
+  },
+  {
+    "revision": "a232888c8024a40c4a45",
+    "url": "/static/js/2.02d427c1.chunk.js"
+  },
+  {
+    "revision": "7f4fdcfa5f60f1070116ba310cc6e4c3",
+    "url": "/static/js/2.02d427c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9370d9801ef23d761b3d",
+    "url": "/static/js/main.df961aca.chunk.js"
+  },
+  {
+    "revision": "208783fa02780d630bac",
+    "url": "/static/js/runtime~main.402cc814.js"
+  },
+  {
+    "revision": "091b47758a91759ceb711a9cb2aee36d",
+    "url": "/static/media/AndrewRametta.091b4775.pdf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
